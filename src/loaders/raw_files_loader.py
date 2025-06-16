2979f1d7dece73f6d863ef75afd2c32090fe8193
miguelamaral254 <miguel.amaral.sales@gmail.com>
2025-06-16 01:45:46 -0300

    refinado raw loader

--- src/loaders/raw_files_loader.py
+++ src/loaders/raw_files_loader.py
@@ -1,24 +1,31 @@
 import gdown
 import zipfile
 import os
+import shutil
 
 def baixar_e_descompactar():
-    # URL do Google Drive
     url = 'https://drive.google.com/uc?export=download&id=1q0Bs4HpPWGVJu_ts0DJd5P_2aYxN1kzw'
+    download_path = "src/data/arquivo.zip"
     
-    # Caminho onde o arquivo será baixado
-    download_path = "src/data/raw/arquivo.zip"
+    os.makedirs(os.path.dirname(download_path), exist_ok=True)
     
-    # Baixando o arquivo
     print(f"Baixando o arquivo de {url}...")
     gdown.download(url, download_path, quiet=False)
     print(f"Arquivo baixado com sucesso em {download_path}")
 
-    # Descompactando o arquivo
     print("Descompactando o arquivo...")
     with zipfile.ZipFile(download_path, 'r') as zip_ref:
-        zip_ref.extractall("src/data/raw")
+        zip_ref.extractall("src/data")
     print("Descompactação concluída.")
+
+    if os.path.exists(download_path):
+        os.remove(download_path)
+        print(f"Arquivo {download_path} deletado com sucesso.")
+    
+    macosx_folder = os.path.join("src", "data", "__MACOSX")
+    if os.path.exists(macosx_folder):
+        shutil.rmtree(macosx_folder)
+        print("Pasta __MACOSX deletada com sucesso.")
 
 if __name__ == "__main__":
     baixar_e_descompactar()
